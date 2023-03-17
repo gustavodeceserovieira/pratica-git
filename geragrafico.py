363585e49871161076275c83cc9bafb86c378935
gustavodeceserovieira <gustavodeceserovieira@gmail.com>
2023-03-17 15:03:21 -0300

    Função principal do programa em novo arquivo

---
+++ geragrafico.py
@@ -0,0 +1,6 @@
+from leitorarquivo import LeitorArquivo
+def main():
+    leitor = LeitorArquivo('data.txt')
+    valores = leitor.getValores()
+    print(valores)
+main()
--- leitorarquivo.py
+++ leitorarquivo.py
@@ -4,9 +4,3 @@ class LeitorArquivo:
         self.valores = [float(x) for x in self.arq.readline().split()]
     def getValores(self):
         return self.valores
-
-
-if __name__ == '__main__':
-    leitor = LeitorArquivo("data.txt")
-    listaValores = leitor.getValores()
-    print(listaValores)    
